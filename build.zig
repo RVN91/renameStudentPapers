0b198aedfaa48db449ecc5d5a3e5afa6a7f097d7
Rasmus Vest Nielsen <rn@civil.aau.dk>
2024-01-31 09:46:39 +0100

    Updated to Zig version 0.11.0
    * Changed build.zig to follow new build procedure

--- build.zig
+++ build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
+    
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -9,19 +10,22 @@ pub fn build(b: *std.build.Builder) void {
 
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable("renameStudentPapers", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
+    const exe = b.addExecutable(.{
+        .name = "renameStudentPapers",
+        .root_source_file = .{ .path = "src/main.zig"},
+        .target = target,
+        .optimize = optimize,
+    });
+        
+    
+    b.installArtifact(exe);
 
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    const run_exe = b.addRunArtifact(exe);
+    
+    const run_step = b.step("run", "Run the application");
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    run_step.dependOn(&run_exe.step);
+
 }
